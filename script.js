960c1d6addcbe9a435ea0fd3dba6f44960097d95
SrinidhiPavushetty <pavushettysrinidhi@gmail.com>
2022-03-11 11:48:26 +0530

    added all

---
+++ script.js
@@ -0,0 +1,21 @@
+// create a variable "Skill Safari"
+var str="skill safari"
+
+//Find length of the string
+console.log(str.length + " is the length is of the string")
+
+//convert the letters of string into capital letters
+console.log(str.toUpperCase())
+
+//What is the index positon of a
+console.log(str.indexOf("a"))
+
+//create a single console that will display a famous conversation between captain america and iron man in avengers. All exchange should be in different lines.
+console.log("Captain America : Big man in a suit of armour. Take that off, what are you?\n Iron Man: Genius, billionaire, playboy, philanthropist.\nCaptain America : I know guys with none of that worth ten of you. I've seen the footage. The only thing you really fight for is yourself. You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you. \nIron Man: I think I would just cut the wire. \nCaptain America : Always a way out... You know, you may not be a threat, but you better stop pretending to be a hero.\nIron Man: A hero? Like you? You're a lab rat, Rogers. Everything special about you came out of a bottle! \nCaptain America : Put on the suit. Let's go a few rounds.")
+
+//Create a Body Mass Index logic by using Height and weight as inputs
+var weight=70
+var height=167
+var bmi=(weight/(height*height)*100*100)
+
+console.log(bmi)
